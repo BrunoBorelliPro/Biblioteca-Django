f36e1ddf586561b34b3de21eb5b26c6f7f64bc38
BrunoBorelliPro <brunoborellipro@gmail.com>
2022-03-20 14:39:41 -0300

    Alterando as views e configurando as urls

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,45 @@
 from django.urls import path
 
-from .views import LivrosCreateView, LivrosListView
+from .views import(
+    LivrosListView,
+    LivroCreateView,
+    LivroDeleteView,
+    LivroUpdateView,
+    LivroDetailView,
+
+    AutorCreateView,
+    AutorListView,
+    AutorDeleteView,
+    AutorUpdateView,
+
+    ReservasListView,
+    UserReservasListView,
+    LivrosDisponiveisListView,
+    ReservaDeleteView,
+    ReservaAdminDeleteView)
 
 app_name = 'core'
 
 urlpatterns = [
-    path('livros', LivrosListView.as_view(), name="livros"),
-    path('livros/createlivro', LivrosCreateView.as_view(), name="createlivro"),
-    path('emprestimos', LivrosListView.as_view(), name="emprestimos"),
-    path('usuarios', LivrosListView.as_view(), name="usuarios"),
+    # Reserva urls
+    path('livros_disponiveis/', LivrosDisponiveisListView.as_view(), name="livros_disponiveis"),
+    path('reservas/', ReservasListView.as_view(), name='visualizar_reservas'),
+    path('minhas_reservas/', UserReservasListView.as_view(), name='minhas_reservas'),
+    path('<pk>/deletar_reserva/', ReservaDeleteView.as_view(), name='deletar_reserva'),
+    path('<pk>/deletar_reserva_admin/', ReservaAdminDeleteView.as_view(), name='deletar_reserva_admin'),
+
+    # Autor urls
+    path('create_autor/', AutorCreateView.as_view(), name='create_autor'),
+    path('list_autor/', AutorListView.as_view(), name='list_autor'),
+    path('<pk>/delete_autor/', AutorDeleteView.as_view(), name='delete_autor'),
+    path('<pk>/update_autor/', AutorUpdateView.as_view(), name='update_autor'),
+
+    # Livro urls
+    path('list_livro/', LivrosListView.as_view(), name="list_livro"),
+    path('create_livro/', LivroCreateView.as_view(), name="create_livro"),
+    path('<pk>/delete_livro/', LivroDeleteView.as_view(), name='delete_livro'),
+    path('<pk>/update_livro/', LivroUpdateView.as_view(), name='update_livro'),
+    path('<pk>/detail_livro/', LivroDetailView.as_view(), name='detail_livro'),
+
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,15 +1,120 @@
-from dataclasses import fields
+from multiprocessing import context
 from pyexpat import model
-from django.views.generic import ListView, CreateView
 
-from core.models import Livro, Emprestimo
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
+
+from core.models import Autor, Livro, Reserva
+from users.models import User
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+from django.utils.decorators import method_decorator
+class LivrosDisponiveisListView(ListView):
+    model = Livro
+    @method_decorator(login_required)
+    def get(self, request):
+        """Lista os livros disponiveis para reserva"""
+        livro_list = self.livros_disponiveis()
+        context = {
+            'livro_list': livro_list,
+            'user': User.objects.get(username=request.user)
+        }
+        return render(request, 'core/livro_list.html', context)
+
+    
+    @method_decorator(login_required)
+    def post(self, request):
+        """Reserva o livro"""
+        livro_id = request.POST['livro_id']
+        livro = Livro.objects.get(id=livro_id)
+        user = User.objects.get(username=request.user)
+        reserva = Reserva(livro = livro, user=user)
+        reserva.save()
+        livro_list = self.livros_disponiveis()
+        context = {
+            'livro_list': livro_list,
+            'user': User.objects.get(username=request.user)
+        }
+        return render(request, 'core/livro_list.html', context)
+
+    def livros_disponiveis(self):
+        return Livro.objects.all().exclude(reserva__reservado=True)
+
+
+
+
+class ReservasListView(PermissionRequiredMixin, ListView):
+    permission_required = 'core.reserva.action_all'
+    model = Reserva
+
+class UserReservasListView(LoginRequiredMixin, ListView):
+    model = Reserva
+    template_name = 'core/minhas_reservas.html'
+    def get_queryset(self):
+        queryset = Reserva.objects.all()
+        queryset = queryset.filter(user=self.request.user)
+        return queryset
+
+
+class ReservaDeleteView(LoginRequiredMixin, DeleteView):
+    model = Reserva
+    success_url = '/biblioteca/minhas_reservas'
+    template_name = "core/delete.html"
+
+class ReservaAdminDeleteView(PermissionRequiredMixin, DeleteView):
+    permission_required = 'core.reserva.action_all'
+
+    model = Reserva
+    success_url = '/biblioteca/reservas/'
+    template_name = "core/delete.html"
+    
+
+class AutorListView(ListView):
+    model = Autor
+    template_name = "core/autor/autor_list.html"
+
+class AutorCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = 'core.autor.action_all'
+    model = Autor
+    fields = '__all__'
+    template_name = "core/autor/autor_form.html"
+    success_url = '/biblioteca/list_autor/'
+
+
+class AutorUpdateView(UpdateView):
+    model = Autor
+    template_name = "core/autor/autor_form.html"
+    fields = '__all__'
+    success_url = '/biblioteca/list_autor/'
+
+
+class AutorDeleteView(DeleteView):
+    model = Autor
+    template_name = "core/delete.html"
+    success_url = '/biblioteca/list_autor/'
+
+
 
 class LivrosListView(ListView):
     model = Livro
+    template_name = 'core/livro/todos_os_livros.html'
 
-class LivrosCreateView(CreateView):
+class LivroCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = 'core.livro.action_all'
+    template_name = "core/livro/livro_form.html"
     model = Livro
     fields = '__all__'
 
-class EmprestimosListView(ListView):
-    model = Emprestimo
+class LivroUpdateView(UpdateView):
+    model = Livro
+    template_name = "core/livro/livro_form.html"
+    fields = '__all__'
+
+class LivroDeleteView(DeleteView):
+    model = Livro
+    template_name = "core/delete.html"
+
+class LivroDetailView(DetailView):
+    model = Livro
+    template_name = "core/livro/detail_livro.html"
+
